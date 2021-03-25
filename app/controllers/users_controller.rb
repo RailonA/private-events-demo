dd2796f5550949b2b0ad4a74a949034c9bcf9cfe
RailonA <railonacosta@gmail.com>
2021-03-25 18:52:34 -0400

    Added events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,25 @@
 class UsersController < ApplicationController
 
-    # def new 
-    #     @user = User.new
-    # end
-    
-    # def create
-    #     @user = User.new
-    # end
+    def index 
+        cookies[:id] = {
+            :value => current_user.id,
+            :expires => 1.week.from_now,
+          }    
 
-    # def show
+        # cookies.delete(:id)
+
+    end
+
+    def new 
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new
+    end
+
+    def show
         
-    # end
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_205102) do
+ActiveRecord::Schema.define(version: 2021_03_25_221333) do
+
+  create_table "events", force: :cascade do |t|
+    t.text "event"
+    t.string "name"
+    t.datetime "date"
+    t.string "location"
+    t.string "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_03_25_205102) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
